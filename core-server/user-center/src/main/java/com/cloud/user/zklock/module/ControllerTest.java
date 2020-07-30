56c7d09c421c317a14e3db1b5a8c15f922b94374
llyJava <429482631@qq.com>
2020-07-30 14:43:35 +0800

    1.curator实现分布式锁，引入依赖 2.创建包zklock和类，具体实现

---
+++ core-server/user-center/src/main/java/com/cloud/user/zklock/module/ControllerTest.java
@@ -0,0 +1,14 @@
+package com.cloud.user.zklock.module;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: TODO
+ * @Author luoliyin
+ * @Date 2020/7/30
+ **/
+@RestController
+public class ControllerTest {
+
+
+}
